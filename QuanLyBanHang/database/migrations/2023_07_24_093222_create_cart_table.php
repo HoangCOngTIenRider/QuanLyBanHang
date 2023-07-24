5e280c783b39a3d03f59cdb5479484f0000e3251
tienhc <tienrider30@gmail.com>
2023-07-24 16:36:38 +0700

    database

---
+++ QuanLyBanHang/database/migrations/2023_07_24_093222_create_cart_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cart', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_user');
+            $table->string('name_product');
+            $table->date('ngay_dat');
+            $table->date('ngay_giao');
+            $table->integer('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cart');
+    }
+};
